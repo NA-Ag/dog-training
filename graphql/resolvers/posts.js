5f6e70daa351e0e5e2414b77f622eddd9ddda38b
Nathan Ag <91394955+NA-Ag@users.noreply.github.com>
2022-05-21 23:26:18 -0500

    Post functionality added

--- graphql/resolvers/posts.js
+++ graphql/resolvers/posts.js
@@ -1,4 +1,4 @@
-const {AuthenticationError} = require("apollo-server");
+const {AuthenticationError, UserInputError} = require("apollo-server");
 
 const Post = require("../../models/Post");
 const checkAuth = require("../../util/checkAuthentification")
@@ -30,9 +30,14 @@ module.exports = {
         Mutation: {
             async createPost(_, {body}, context){
                 const user = checkAuth(context);
+
+                if(args.body.trim() === ""){
+                    throw new Error("Post body cannot be empty");
+                }
+
                 const newPost = new Post ({
                     body,
-                    user: user.indexOf, 
+                    user: user.id, 
                     username: user.username,
                     createdAt: new Date().toISOString()
                 });
@@ -43,15 +48,32 @@ module.exports = {
                 const user = checkAuth(context);
                 try{
                     const post = await Post.findById(postId);
-                    if (user.username === post.usernam){
+                    if (user.username === post.username){
                         await post.delete();
-                        return "Post deletec succesfully";
+                        return "Post deleted succesfully";
                     } else {
                         throw new AuthenticationError("Action not allowed");
                     }
                 } catch(err){
                     throw new Error(err);
                 }
+            },
+            async likePost(_, {postId}, context){
+                const {username} = checkAuth(context);
+
+                const post = await Post.findById(postId);
+                if(post){
+                    if(post.likes.find(like=>like.username===usernam)){
+                        post.likes = post.likes.filter(like=>like.username !== username); 
+                    } else {
+                        post.likes.push({
+                            username,
+                            createdAt: new Date().toISOString()
+                        })
+                    }
+                    await post.save();
+                    return post;
+                } else throw new UserInputError("Post not found");
             }
         } 
 };
