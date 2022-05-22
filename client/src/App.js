cf40214d1899c9543a90cd40294e56bcf7f09d4f
Nathan Ag <91394955+NA-Ag@users.noreply.github.com>
2022-05-22 02:05:11 -0500

    Added navigation bar, pending Semantic-UI fix

---
+++ client/src/App.js
@@ -0,0 +1,23 @@
+import React from "react"
+import { BrowserRouter as Router, Route } from "react-router-dom";
+
+import "semantic-ui-css/semantic.min.css";
+import './App.css';
+
+import MenuBar from "./Components/navMenuBar";
+import Home from "./pages/Home";
+import Login from "./pages/Login";
+import SignUp from "./pages/SignUp";
+
+function App() {
+  return (
+    <Router>
+    <MenuBar/>
+    <Route exact path="/" component={Home}/>
+    <Route exact path="/login" component={Login}/>
+    <Route exact path="/signup" component={SignUp}/>
+    </Router>
+  );
+}
+
+export default App;
